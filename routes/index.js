bc02caf61d829f338cc7bd4fd15a2b83ab44f6bc
noemivega <noemivega1305@gmail.com>
2020-10-17 09:42:30 -0500

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var nombres = ['Andrea', 'Brenda','Carlos','Daniela','Eduardo', 'Fernando','Gabriela','Humberto','Ilian','Jorge'];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { page: 'Home', menuId: 'home'});
+  res.render('index', { page: 'Home', menuId:'home'});
 });
 
 /*router.get('/', function(req, res, next){
   res.render('index', {title:'Lista de nombres', nombres: nombres});
-});*/
+});
 
 router.post('/',function(req,res){
   res.send('Tengo una petición con POST');
@@ -21,6 +21,6 @@ router.put('/greeting', function(req,res){
 
 router.delete('/hello', function(req,res){
   res.send('Te doy un saludo con DELETE');
-});
+});*/
 
 module.exports = router;
